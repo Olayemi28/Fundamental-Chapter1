8560eb5b3e5e19bc72f629e0a4d04871029fe529
Olayemi28 <oyekunle.amotul.fareed2004@gmail.com>
2023-09-11 13:26:43 +0100

    intro

---
+++ Question 10/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Question_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that prints the first 100 members of the sequence 2, -
+            // 3, 4, -5, 6, -7, 8.
+
+            for(int i = 2; i <=100; i++)
+            {
+                if(i % 2 == 1)
+                {
+                    Console.WriteLine($"-{i}");
+                }
+               else
+               {
+                    Console.WriteLine(i);
+               }
+            }
+        }
+    }
+}
